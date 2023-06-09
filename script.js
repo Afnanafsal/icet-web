b61fbe8189660f519841aa30393b5bccde11a2c5
AFNAN AFSAL <afnnafsal@gmail.com>
2023-06-09 18:56:04 +0530

    new project started

---
+++ script.js
@@ -0,0 +1,13 @@
+// Optional: Close the dropdown when clicking outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+      var dropdowns = document.getElementsByClassName('dropdown-content');
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.style.display === 'block') {
+          openDropdown.style.display = 'none';
+        }
+      }
+    }
+  };
+  
