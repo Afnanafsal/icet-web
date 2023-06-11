5dd9c7a650131d998a847d16f390549a936dfa4e
Afnan Afsal <87473921+Afnanafsal@users.noreply.github.com>
2023-06-11 10:15:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// Optional: Close the dropdown when clicking outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropdown-btn')) {
-      var dropdowns = document.getElementsByClassName('dropdown-content');
-      for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.style.display === 'block') {
-          openDropdown.style.display = 'none';
-        }
-      }
-    }
-  };
-  
+
+$(document).ready(function() {
+  $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+  });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+  if($(window).scrollTop()) {
+        $('nav').addClass('black');
+  }
+
+  else {
+        $('nav').removeClass('black');
+  }
+})
